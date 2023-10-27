919257a00c530d38c892550f30b7e3205d1dca03
sydneykillilea1 <killi167@umn.edu>
2023-10-26 19:59:58 -0500

    clockwise and counterclockwise

--- hardware/motor-control.py
+++ hardware/motor-control.py
@@ -8,12 +8,20 @@ GPIO.setup(ledpin,GPIO.OUT)
 pi_pwm = GPIO.PWM(ledpin,1000)		#create PWM instance with frequency
 pi_pwm.start(0)				#start PWM of required Duty Cycle 
 while True:
-    for duty in range(0,101,1):
-        pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
-        sleep(0.01)
-    sleep(0.5)
-    
-    for duty in range(100,-1,-1):
-        pi_pwm.ChangeDutyCycle(duty)
-        sleep(0.01)
-    sleep(0.5)
+    if GPIO.input(16) == GPIO.HIGH:
+        for duty in range(0,101,1):
+            pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
+            sleep(0.01)
+        sleep(0.5)
+        for duty in range(100,-1,-1):
+            pi_pwm.ChangeDutyCycle(duty)
+            sleep(0.01)
+    if GPIO.input(20) == GPIO.HIGH:
+        for duty in range(100,-1,-1):
+            pi_pwm.ChangeDutyCycle(duty)
+            sleep(0.01)
+        sleep(0.5)
+        for duty in range(0,101,1):
+            pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
+            sleep(0.01)
+        sleep(0.5)
